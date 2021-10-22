33a67725457e53851cfafea09a067a1e3b57f467
Mohit Parashar <mohit1672002@gmail.com>
2021-10-22 20:49:48 +0530

    initial commit

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-  "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
-  "x-rapidapi-host": process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+  // "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
+  "x-rapidapi-key": "fc5068306dmsh4435d24d784ce78p17761ajsnd5f5236fb9d3",
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  // "x-rapidapi-host": process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
 };
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,9 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoNewsHeaders = {
+  // "x-bingapis-sdk": "true",
+  // "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
+  // "x-rapidapi-host": process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
   "x-bingapis-sdk": "true",
-  "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
-  "x-rapidapi-host": process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "x-rapidapi-key": "fc5068306dmsh4435d24d784ce78p17761ajsnd5f5236fb9d3",
 };
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
